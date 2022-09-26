d4e3d180bdbf608f2f52523c1830fe657578dc76
Scott Willrich <swillrich@gmail.com>
2022-09-25 19:09:51 -0500

    added count and average rating plus sorting query results

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.db.models import Avg
 
 from .models import Book
 
 # Create your views here.
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("-title")
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg("rating")) # rating__avg
     return render(request, "book_outlet/index.html", {
         "books": books,
+        "total_number_of_books": num_books,
+        "average_rating": avg_rating
 })
 
     
